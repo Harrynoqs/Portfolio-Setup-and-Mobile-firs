f5d2226f50ed42b719a7fdd4447864ba514243d6
Harrynoqs <109760801+Harrynoqs@users.noreply.github.com>
2022-09-16 14:45:43 +0300

    add presevedata.js

--- preservedata.js
+++ preservedata.js
@@ -14,19 +14,3 @@ submitBtn.addEventListener('click', () => {
   text = text.value;
   localStorage.setItem('text', text);
 });
->>>>>>> 9ed87d52ff2e0e7dfdecf690dc702331a00f9af8
-
-submitBtn.addEventListener('click', () => {
-  let name = document.getElementById('name');
-  let email = document.getElementById('email');
-  let text = document.getElementById('text');
-
-  name = name.value;
-  localStorage.setItem('name', name);
-
-  email = email.value;
-  localStorage.setItem('email', email);
-
-  text = text.value;
-  localStorage.setItem('text', text);
-});
